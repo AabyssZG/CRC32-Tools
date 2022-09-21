e88e7e3b2feb51dbc7151449cab8e4fcfe143e87
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2022-09-21 09:38:19 +0800

    Update ReadZip.py

--- SingleFile_Version/ReadZip.py
+++ SingleFile_Version/ReadZip.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3.8
 # -*- coding: utf-8 -*-
-import requests
 import zipfile
 
 def title():
     print('+-----------------------------------------------------+')
     print('+              渊龙Sec安全团队CTF工具包               +')
     print('+              团队公开QQ群：877317946                +')
-    print('+             Title: CRC-Tools,so easy!!!             +')
+    print('+               Title: CRC-Tools_ReadZIP              +')
     print('+     python3 ReadZip.py --> ReadZip >>> Demo.zip     +')
     print('+                作者：曾哥（AabyssZG）               +')
-    print('+                  版本：V1.1优化版                   +')
+    print('+                 版本：V1.3单文件版                  +')
     print('+-----------------------------------------------------+')
+
+def FileRead(zipname):
+    try:
+    	f =open(zipname)                               #打开目标文件
+    	f.close()
+    except FileNotFoundError:
+    	print ("未找到同目录下的压缩包文件" + zipname) #如果未找到文件，输出错误
+    	return                                         #退出线程，进行详细报错
+    except PermissionError:
+    	print ("无法读取目标压缩包（无权限访问）")     #如果发现目标文件无权限，输出错误
+    	return                                         #退出线程，进行详细报错
 
 def ReadCRC(zipname):
     zip_url = "./" + zipname
@@ -31,4 +41,10 @@ def ReadCRC(zipname):
 if __name__ == '__main__':
     title()
     zipname = str(input("请输入压缩包名字：\nReadZip >>> "))
-    ReadCRC(zipname)
+    try:
+        if zipname:
+        	FileRead(zipname)
+        	ReadCRC(zipname)
+    except BaseException as e:
+        err = str(e)
+        print('脚本详细报错：' + err)
