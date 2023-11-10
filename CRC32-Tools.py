1b17509515e0e4625618245b894610c67c71ad84
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-11-10 10:41:19 +0800

    Update CRC32-Tools.py

--- CRC32-Tools.py
+++ CRC32-Tools.py
@@ -4,7 +4,7 @@ import zipfile
 import argparse
 import string
 import binascii
-import io
+import io, sys
 
 def title():
     print('+-----------------------------------------------------+')
@@ -13,19 +13,19 @@ def title():
     print('+            Title: CRC32-Tools,so easy!!!            +')
     print('+  python3 CRC32-Tools.py -h --> 根据不同情况选择参数 +')
     print('+                作者：曾哥（AabyssZG）               +')
-    print('+                  版本：V2.2整合版                   +')
+    print('+                  版本：V2.3整合版                   +')
     print('+-----------------------------------------------------+')
 
 def FileRead(zipname):
     try:
-    	f =open(zipname)                               #打开目标文件
-    	f.close()
+        f =open(zipname)                               #打开目标文件
+        f.close()
     except FileNotFoundError:
-    	print ("未找到同目录下的压缩包文件" + zipname) #如果未找到文件，输出错误
-    	return                                         #退出线程，进行详细报错
+        print ("未找到同目录下的压缩包文件" + zipname) #如果未找到文件，输出错误
+        return                                         #退出线程，进行详细报错
     except PermissionError:
-    	print ("无法读取目标压缩包（无权限访问）")     #如果发现目标文件无权限，输出错误
-    	return                                         #退出线程，进行详细报错
+        print ("无法读取目标压缩包（无权限访问）")     #如果发现目标文件无权限，输出错误
+        return                                         #退出线程，进行详细报错
 
 def ReadCRC(zipname):
     zip_url = "./" + zipname
@@ -34,9 +34,9 @@ def ReadCRC(zipname):
     crc_list = []
     print('+--------------遍历指定压缩包的CRC值----------------+')
     for name in name_list:
-    	name_message = file_zip.getinfo(name)
-    	crc_list.append(hex(name_message.CRC))
-    	print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
+        name_message = file_zip.getinfo(name)
+        crc_list.append(hex(name_message.CRC))
+        print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
     print('+---------------------------------------------------+')
     crc32_list = str(crc_list)
     crc32_list = crc32_list.replace('\'' , '')
@@ -50,10 +50,10 @@ def OneByte(zipname):
     crc32_list = []
     print('+--------------遍历指定压缩包的CRC值----------------+')
     for name in name_list:
-    	name_message = file_zip.getinfo(name)
-    	crc_list.append(name_message.CRC)
-    	crc32_list.append(hex(name_message.CRC))
-    	print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
+        name_message = file_zip.getinfo(name)
+        crc_list.append(name_message.CRC)
+        crc32_list.append(hex(name_message.CRC))
+        print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
     print('+-------------对输出的CRC值进行碰撞-----------------+')
     comment = ''
     chars = string.printable
@@ -69,9 +69,9 @@ def OneByte(zipname):
     crc32_list = crc32_list.replace('\'' , '')
     print("读取成功，导出CRC列表为：" + crc32_list)                     #导出CRC列表
     if comment:
-    	print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
+        print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
     else:
-      print('CRC碰撞没有结果，请检查压缩包内文件是否为1Byte！！！')
+        print('CRC碰撞没有结果，请检查压缩包内文件是否为1Byte！！！')
 
 def TwoByte(zipname):
     zip_url = "./" + zipname
@@ -81,10 +81,10 @@ def TwoByte(zipname):
     crc32_list = []
     print('+--------------遍历指定压缩包的CRC值----------------+')
     for name in name_list:
-    	name_message = file_zip.getinfo(name)
-    	crc_list.append(name_message.CRC)
-    	crc32_list.append(hex(name_message.CRC))
-    	print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
+        name_message = file_zip.getinfo(name)
+        crc_list.append(name_message.CRC)
+        crc32_list.append(hex(name_message.CRC))
+        print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
     print('+-------------对输出的CRC值进行碰撞-----------------+')
     comment = ''
     chars = string.printable
@@ -102,9 +102,9 @@ def TwoByte(zipname):
     crc32_list = crc32_list.replace('\'' , '')
     print("读取成功，导出CRC列表为：" + crc32_list)                     #导出CRC列表
     if comment:
-    	print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
+        print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
     else:
-      print('CRC碰撞没有结果，请检查压缩包内文件是否为2Byte！！！')
+        print('CRC碰撞没有结果，请检查压缩包内文件是否为2Byte！！！')
 
 def ThreeByte(zipname):
     zip_url = "./" + zipname
@@ -114,31 +114,39 @@ def ThreeByte(zipname):
     crc32_list = []
     print('+--------------遍历指定压缩包的CRC值----------------+')
     for name in name_list:
-    	name_message = file_zip.getinfo(name)
-    	crc_list.append(name_message.CRC)
-    	crc32_list.append(hex(name_message.CRC))
-    	print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
+        name_message = file_zip.getinfo(name)
+        crc_list.append(name_message.CRC)
+        crc32_list.append(hex(name_message.CRC))
+        print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
     print('+-------------对输出的CRC值进行碰撞-----------------+')
     comment = ''
     chars = string.printable
-    for crc_value in crc_list:
-        for char1 in chars:
-            for char2 in chars:
-                for char3 in chars:
-                    res_char = char1 + char2 + char3                #获取任意3Byte字符
-                    thicken_crc = binascii.crc32(res_char.encode()) #获取任意3Byte字符串的CRC32值
-                    calc_crc = thicken_crc & 0xffffffff             #将任意3Byte字符串的CRC32值与0xffffffff进行与运算
+    result_dict={}
+    for char1 in chars:
+        for char2 in chars:
+            for char3 in chars:
+                res_char = char1 + char2 + char3                #获取任意3Byte字符
+                thicken_crc = binascii.crc32(res_char.encode()) #获取任意3Byte字符串的CRC32值
+                calc_crc = thicken_crc & 0xffffffff             #将任意3Byte字符串的CRC32值与0xffffffff进行与运算
+                for crc_value in crc_list:
                     if calc_crc == crc_value:                       #匹配两个CRC32值
-                        print('[Success] {}: {}'.format(hex(crc_value),res_char))
-                        comment += res_char
+                        index = crc32_list.index(hex(crc_value))
+                        num = int(index)
+                        new_data = {num : res_char}
+                        print('[Success] 第 {} 个文件 {}: {}'.format(num,hex(crc_value),res_char))
+                        result_dict.update(new_data)
+                        break
+    sorted_items = sorted(result_dict.items())
+    for key, res_char in sorted_items:
+        comment += res_char
     print('+-----------------CRC碰撞结束！！！-----------------+')
     crc32_list = str(crc32_list)
     crc32_list = crc32_list.replace('\'' , '')
     print("读取成功，导出CRC列表为：" + crc32_list)                     #导出CRC列表
     if comment:
-    	print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
+        print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
     else:
-      print('CRC碰撞没有结果，请检查压缩包内文件是否为3Byte！！！')
+        print('CRC碰撞没有结果，请检查压缩包内文件是否为3Byte！！！')
 
 def FourByte(zipname):
     zip_url = "./" + zipname
@@ -148,32 +156,40 @@ def FourByte(zipname):
     crc32_list = []
     print('+--------------遍历指定压缩包的CRC值----------------+')
     for name in name_list:
-    	name_message = file_zip.getinfo(name)
-    	crc_list.append(name_message.CRC)
-    	crc32_list.append(hex(name_message.CRC))
-    	print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
+        name_message = file_zip.getinfo(name)
+        crc_list.append(name_message.CRC)
+        crc32_list.append(hex(name_message.CRC))
+        print('[OK] {0}: {1}'.format(name,hex(name_message.CRC)))
     print('+-------------对输出的CRC值进行碰撞-----------------+')
     comment = ''
     chars = string.printable
-    for crc_value in crc_list:
-        for char1 in chars:
-            for char2 in chars:
-                for char3 in chars:
-                    for char4 in chars:
-                        res_char = char1 + char2 + char3 + char4        #获取任意4Byte字符
-                        thicken_crc = binascii.crc32(res_char.encode()) #获取任意4Byte字符串的CRC32值
-                        calc_crc = thicken_crc & 0xffffffff             #将任意4Byte字符串的CRC32值与0xffffffff进行与运算
+    result_dict={}
+    for char1 in chars:
+        for char2 in chars:
+            for char3 in chars:
+                for char4 in chars:
+                    res_char = char1 + char2 + char3 + char4        #获取任意4Byte字符
+                    thicken_crc = binascii.crc32(res_char.encode()) #获取任意4Byte字符串的CRC32值
+                    calc_crc = thicken_crc & 0xffffffff             #将任意4Byte字符串的CRC32值与0xffffffff进行与运算
+                    for crc_value in crc_list:
                         if calc_crc == crc_value:                       #匹配两个CRC32值
-                            print('[Success] {}: {}'.format(hex(crc_value),res_char))
-                            comment += res_char
+                            index = crc32_list.index(hex(crc_value))
+                            num = int(index)
+                            new_data = {num : res_char}
+                            print('[Success] 第 {} 个文件 {}: {}'.format(num,hex(crc_value),res_char))
+                            result_dict.update(new_data)
+                            break
+    sorted_items = sorted(result_dict.items())
+    for key, res_char in sorted_items:
+        comment += res_char
     print('+-----------------CRC碰撞结束！！！-----------------+')
     crc32_list = str(crc32_list)
     crc32_list = crc32_list.replace('\'' , '')
     print("读取成功，导出CRC列表为：" + crc32_list)                     #导出CRC列表
     if comment:
-    	print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
+        print('CRC碰撞成功，结果为: {}'.format(comment))                  #输出CRC碰撞结果
     else:
-      print('CRC碰撞没有结果，请检查压缩包内文件是否为4Byte！！！')
+        print('CRC碰撞没有结果，请检查压缩包内文件是否为4Byte！！！')
 
 if __name__ == '__main__':
     title()
@@ -187,20 +203,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
     try:
         if args.readzip:
-        	  FileRead(args.readzip)
-        	  ReadCRC(args.readzip)
+            FileRead(args.readzip)
+            ReadCRC(args.readzip)
         if args.onebyte:
-        	  FileRead(args.onebyte)
-        	  OneByte(args.onebyte)
+            FileRead(args.onebyte)
+            OneByte(args.onebyte)
         if args.twobyte:
-        	  FileRead(args.twobyte)
-        	  TwoByte(args.twobyte)
+            FileRead(args.twobyte)
+            TwoByte(args.twobyte)
         if args.threebyte:
-        	  FileRead(args.threebyte)
-        	  ThreeByte(args.threebyte)
+            FileRead(args.threebyte)
+            ThreeByte(args.threebyte)
         if args.fourbyte:
-        	  FileRead(args.fourbyte)
-        	  FourByte(args.fourbyte)
+            FileRead(args.fourbyte)
+            FourByte(args.fourbyte)
+    except KeyboardInterrupt:
+        print("Ctrl + C 手动终止了进程")
+        sys.exit()
     except BaseException as e:
         err = str(e)
         print('脚本详细报错：' + err)
+        sys.exit(0)
